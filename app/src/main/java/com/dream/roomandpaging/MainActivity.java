7e946f3fe1219d1d1aac708eeb5997e2858edf8e
xingxinglv <lvxingxing@kingsoft.com>
2020-05-30 16:32:48 +0800

    add flag

--- app/src/main/java/com/dream/roomandpaging/MainActivity.java
+++ app/src/main/java/com/dream/roomandpaging/MainActivity.java
@@ -20,6 +20,7 @@ import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private int flag = 1;
 
     private RecyclerView recy;
     private PeopleAdapter peopleAdapter;
